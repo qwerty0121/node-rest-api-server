84a5f992b776c4a82ce738dbc7f8b6b226784974
qwerty0121 <sh1ng0718o@gmail.com>
2023-03-11 22:27:06 +0900

    [add] GET用APIの追加

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ app.listen(port, () => console.log(`Listening on port ${port}...`))
 app.get('/', (req, res) => {
   res.send('Simple REST API')
 })
+
+const courses = [
+  { id: 1, name: 'computer science'},
+  { id: 2, name: 'information technology'},
+  { id: 3, name: 'business intelligence'}
+]
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
+})
